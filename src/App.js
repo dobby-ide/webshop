5482ef7939bc13ff1ae937b2e15c5e86ae26b03d
spavythra <spavythra@gmail.com>
2022-02-22 13:53:13 +0200

    (App.js): Navbar component added. component folder has been added under src. Navbar structure has been created.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
 import Products from './Products';
 
 function App() {
   return (
     <div className="container">
+      <Navbar/>
       <Products />
     </div>
   );
