da89ddfa1b5c8b9672ce8e0d48f52d88e5308981
Fabio Privitera <fabio.privitera@tuni.fi>
2022-02-22 18:21:08 +0200

    App.js:UseState dependency add; useState hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
 import Products from './Products';
-
+import { useState, useEffect } from 'react';
 function App() {
+  const [productsisVisible, setProductsIsVisible] = useState(true);
+  const [womenClothingisVisible, setWomenClothingIsVisible] = useState(false);
+  const [jewelryisVisible, setJewelryIsVisible] = useState(false);
+  const [menClothingisVisible, setMenClothingIsVisible] = useState(false);
+  const [electronicsisVisible, setElectronicsIsVisible] = useState(false);
   return (
     <div className="container">
       <Products />
