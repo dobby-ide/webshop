1d6ee10ffe814bb5ab7a5559255699e6f23f38fa
Fabio Privitera <fabio.privitera@tuni.fi>
2022-02-22 20:21:10 +0200

    minor changes to App.js

--- src/App.js
+++ src/App.js
@@ -3,39 +3,44 @@ import './App.css';
 import { useAsync } from 'react-async';
 import Products from './Products';
 import Filter from './Filter';
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import WomenClothes from './WomenClothes';
 import Jewelry from './Jewelry';
 import MenClothes from './MenClothes';
 import Electronics from './Electronics';
+
+
+
+//useState to hide/show components
+
 function App() {
   const [productsisVisible, setProductsIsVisible] = useState(true);
   const [womenClothingisVisible, setWomenClothingIsVisible] = useState(false);
   const [jewelryisVisible, setJewelryIsVisible] = useState(false);
-  const [menClothingisVisible, setMenClothingIsVisible] = useState(false);
-  const [electronicsisVisible, setElectronicsIsVisible] = useState(false);
-
-  const onAllProductsVisibility = () => {
+   const [menClothingisVisible, setMenClothingIsVisible] = useState(false);
+   const [electronicsisVisible, setElectronicsIsVisible] = useState(false);
+ 
+ const onAllProductsVisibility=()=>{
     setProductsIsVisible(true);
     setWomenClothingIsVisible(false);
     setJewelryIsVisible(false);
     setMenClothingIsVisible(false);
     setElectronicsIsVisible(false);
-  };
-  const onWomenVisibility = () => {
+ }
+   const onWomenVisibility =()=>{
     setProductsIsVisible(false);
     setWomenClothingIsVisible(true);
-    setJewelryIsVisible(false);
+    setJewelryIsVisible(false)
     setMenClothingIsVisible(false);
-    setElectronicsIsVisible(false);
-  };
-  const onJewelryVisibility = () => {
+    setElectronicsIsVisible(false)
+  }
+  const onJewelryVisibility=()=>{
     setJewelryIsVisible(true);
-    setElectronicsIsVisible(false);
+    setElectronicsIsVisible(false)
     setProductsIsVisible(false);
     setWomenClothingIsVisible(false);
     setMenClothingIsVisible(false);
-  };
+  }
   const onMenVisibility = () => {
     setElectronicsIsVisible(false);
     setMenClothingIsVisible(true);
@@ -43,13 +48,13 @@ function App() {
     setProductsIsVisible(false);
     setWomenClothingIsVisible(false);
   };
-  const onElectronicsVisibilityy = () => {
-    setElectronicsIsVisible(true);
-    setMenClothingIsVisible(false);
-    setJewelryIsVisible(false);
-    setProductsIsVisible(false);
-    setWomenClothingIsVisible(false);
-  };
+  const onElectronicsVisibilityy=()=>{
+  setElectronicsIsVisible(true);
+  setMenClothingIsVisible(false);
+  setJewelryIsVisible(false);
+  setProductsIsVisible(false);
+  setWomenClothingIsVisible(false);
+  }
   return (
     <div className="container">
       <Filter
