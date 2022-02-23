346cbac6f175f130ce45cf8422b05ce8196ee0d8
spavythra <spavythra@gmail.com>
2022-02-23 15:07:02 +0200

    (Footer.js): Footer component has been created, added in App.js at line no-80

---
+++ src/Components/Footer.js
@@ -0,0 +1,108 @@
+
+  import styled from "styled-components";
+  
+  const Container = styled.div`
+    display: flex;
+    
+  `;
+  
+  const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+  `;
+  
+  const Logo = styled.h1``;
+  
+  const Desc = styled.p`
+    margin: 20px 0px;
+  `;
+  
+  const SocialContainer = styled.div`
+    display: flex;
+  `;
+  
+  const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${(props) => props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+  `;
+  
+  const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+    
+  `;
+  
+  const Title = styled.h3`
+    margin-bottom: 30px;
+  `;
+  
+  const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+  `;
+  
+  const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+  `;
+  
+  const Right = styled.div`
+    flex: 1;
+    padding: 20px;
+   
+  `;
+  
+  const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+  `;
+  
+  const Payment = styled.img`
+      width: 50%;
+  `;
+  
+  const Footer = () => {
+    return (
+      <Container>
+        <Left>
+          <Logo>CLOTHS.</Logo>
+          <Desc>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Desc>
+        </Left>
+        <Center>
+          <Title>Useful Links</Title>
+          <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Cart</ListItem>
+            <ListItem>Man Fashion</ListItem>
+            <ListItem>Woman Fashion</ListItem>
+            <ListItem>Accessories</ListItem>
+            <ListItem>My Account</ListItem>
+            <ListItem>Order Tracking</ListItem>
+            <ListItem>Wishlist</ListItem>
+            <ListItem>Wishlist</ListItem>
+            <ListItem>Terms</ListItem>
+          </List>
+        </Center>
+        <Right>
+          
+        </Right>
+      </Container>
+    );
+  };
+  
+  export default Footer;
