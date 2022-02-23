23e6f5d0aa721faea535bb48a174af6fad3fa9bf
spavythra <spavythra@gmail.com>
2022-02-23 15:26:55 +0200

    (Footer.js): icons added, styling corrected with the App.css file

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,27 +1,14 @@
-
+import {
+    Facebook,
+    Instagram,
+    MailOutline,
+    Phone,
+    Pinterest,
+    Room,
+    Twitter,
+  } from "@material-ui/icons";
   import styled from "styled-components";
-  
-  const Container = styled.div`
-    display: flex;
-    
-  `;
-  
-  const Left = styled.div`
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    padding: 20px;
-  `;
-  
-  const Logo = styled.h1``;
-  
-  const Desc = styled.p`
-    margin: 20px 0px;
-  `;
-  
-  const SocialContainer = styled.div`
-    display: flex;
-  `;
+  import '../App.css'
   
   const SocialIcon = styled.div`
     width: 40px;
@@ -35,57 +22,41 @@
     margin-right: 20px;
   `;
   
-  const Center = styled.div`
-    flex: 1;
-    padding: 20px;
-    
-  `;
-  
-  const Title = styled.h3`
-    margin-bottom: 30px;
-  `;
-  
-  const List = styled.ul`
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    display: flex;
-    flex-wrap: wrap;
-  `;
-  
   const ListItem = styled.li`
     width: 50%;
     margin-bottom: 10px;
   `;
-  
-  const Right = styled.div`
-    flex: 1;
-    padding: 20px;
-   
-  `;
-  
-  const ContactItem = styled.div`
-    margin-bottom: 20px;
-    display: flex;
-    align-items: center;
-  `;
-  
+
   const Payment = styled.img`
       width: 50%;
   `;
   
   const Footer = () => {
     return (
-      <Container>
-        <Left>
-          <Logo>CLOTHS.</Logo>
-          <Desc>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </Desc>
-        </Left>
-        <Center>
-          <Title>Useful Links</Title>
-          <List>
+      <div className="footer-container">
+        <div className="footer-left">
+          <div className="footer-logo"><h1>CLOTHS.</h1></div>
+          <p className="footer-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          </p>
+          <div className="footer-social">
+            <SocialIcon color="3B5999">
+              <Facebook />
+            </SocialIcon>
+            <SocialIcon color="E4405F">
+              <Instagram />
+            </SocialIcon>
+            <SocialIcon color="55ACEE">
+              <Twitter />
+            </SocialIcon>
+            <SocialIcon color="E60023">
+              <Pinterest />
+            </SocialIcon>
+          </div>
+        </div>
+        <div className="footer-center">
+          <h3 style={{marginBottom: '30px'}}>Useful Links</h3>
+          <ul className="footer-list">
             <ListItem>Home</ListItem>
             <ListItem>Cart</ListItem>
             <ListItem>Man Fashion</ListItem>
@@ -96,12 +67,22 @@
             <ListItem>Wishlist</ListItem>
             <ListItem>Wishlist</ListItem>
             <ListItem>Terms</ListItem>
-          </List>
-        </Center>
-        <Right>
-          
-        </Right>
-      </Container>
+          </ul>
+        </div>
+        <div className="footer-right">
+        <h3 style={{marginBottom: '30px'}}>Contact</h3>
+          <div className="footer-contact">
+            <Room style={{marginRight:"10px"}}/> 000 ABC def , Tampere 0000
+          </div>
+          <div className="footer-contact">
+            <Phone style={{marginRight:"10px"}}/> +1 234 56 78
+            </div>
+          <div className="footer-contact">
+            <MailOutline style={{marginRight:"10px"}} /> contact@cloths.come
+            </div>
+          {/* <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" /> */}
+        </div>
+      </div>
     );
   };
   
