6294a12e2e1153dc954ca22de818a96a1176b40e
Fabio Privitera <fabio.privitera@tuni.fi>
2022-02-24 15:55:14 +0200

    Products.js: modified, new component import and put in render(Card.js)

--- src/Products.js
+++ src/Products.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import { useAsync } from 'react-async';
-
+import Card from './Card';
 // Then we'll fetch user data from this API
 const loadUsers = async () =>
   await fetch('https://fakestoreapi.com/products')
@@ -12,52 +12,69 @@ const loadUsers = async () =>
 function Products() {
   const { data, error, isLoading } = useAsync({ promiseFn: loadUsers });
   const [cart, setCart] = useState([]);
-  const url = "http://localhost:3010/cart"
+  const url = 'http://localhost:3010/cart';
   //For saving in cart.json file
-  function saveToCart(products){
-    const options = {method:'POST',
-                    headers:{'Content-type':'application/json'},
-                    body:JSON.stringify({id:products.id, title:products.title, price:products.price, description:products.description, category:products.category, image:products.image, quantity:"1"} )
-                   
-                    }
-  fetch(url, options)
-    .then(response=>response.json())
-    .then(data=>{
-      console.log(data.id + "Added successfully");
-       setCart([...cart, data.id]);
-    })
+  function saveToCart(products) {
+    const options = {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        id: products.id,
+        title: products.title,
+        price: products.price,
+        description: products.description,
+        category: products.category,
+        image: products.image,
+        quantity: '1',
+      }),
+    };
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.id + 'Added successfully');
+        setCart([...cart, data.id]);
+      });
   }
   //Fetches product in the cart
-    const getCart = () =>{
-    console.log("here");
+  const getCart = () => {
+    console.log('here');
     fetch(url)
-    .then(response => response.json())   
-    .then((cartList)=>setCart(cartList.map(item=>item.id)))    
-    
-  }
-  useEffect(()=>{getCart()}, [true]);
-  
+      .then((response) => response.json())
+      .then((cartList) => setCart(cartList.map((item) => item.id)));
+  };
+  useEffect(() => {
+    getCart();
+  }, [true]);
 
   if (isLoading) return 'Loading...';
   if (error) return `Something went wrong: ${error.message}`;
   if (data)
     // The rendered component
     return (
-      <div className="container">
-        <div>
-          <h2>products</h2>
-        </div>
-        {data.map((products) => (
-          <div key={products.id} className="row">
-            <div className="col-md-12">
-              <p>{products.title}</p>
-              <p>{products.price}</p>
-              <p><button disabled={cart.some(cartId => cartId===products.id)} onClick={()=>saveToCart(products)}>Add To Cart</button></p>
-              <img src={products.image} className="products_img" />
-            </div>
+      <Card className="products_card">
+        <div className="container">
+          <div>
+            <h2>products</h2>
           </div>
-        ))}
-      </div>
+          {data.map((products) => (
+            <div key={products.id} className="row">
+              <div className="col-md-12">
+                <p>{products.title}</p>
+                <p>{products.price}</p>
+                <p>
+                  <button
+                    disabled={cart.some((cartId) => cartId === products.id)}
+                    onClick={() => saveToCart(products)}
+                  >
+                    Add To Cart
+                  </button>
+                </p>
+                <img src={products.image} className="products_img" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </Card>
     );
 }
 
